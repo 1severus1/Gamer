e64d9e1dc8484bd903c981e90af3a987b5b81064
severus <sf_lxyx@163.com>
2023-03-12 23:16:41 +0800

    Move toward camera way & Controller Rotation

--- ActionRogueLike/Source/ActionRogueLike/Private/SCharacter.cpp
+++ ActionRogueLike/Source/ActionRogueLike/Private/SCharacter.cpp
@@ -3,6 +3,7 @@
 
 #include "SCharacter.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
@@ -10,9 +11,12 @@ ASCharacter::ASCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	bUseControllerRotationYaw=false;
+	GetCharacterMovement()->bOrientRotationToMovement=true;
 	
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SringArmComp");
-	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->SetupAttachment(RootComponent); 
+	SpringArmComp->bUsePawnControlRotation=true;
 	
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
 	CameraComp->SetupAttachment(SpringArmComp);
@@ -28,12 +32,21 @@ void ASCharacter::BeginPlay()
 
 void ASCharacter::MoveForward(float value)
 {
-	AddMovementInput(GetActorForwardVector(),value);
+	// The spring arm use the control rot so do the camera
+	// which make character move toward the camera forward
+	FRotator CtrlRot=GetControlRotation();
+	// only care about Yaw. Roll did nothing in getting forward vecter
+	CtrlRot.Pitch=0.0f;
+	AddMovementInput(CtrlRot.Vector(),value);
 }
 
 void ASCharacter::MoveRight(float value)
 {
-	AddMovementInput(GetActorRightVector(),value);
+	FRotator CtrlRot=GetControlRotation();
+	// compare with MoveForward, Roll will effect the Right vector
+	CtrlRot.Pitch=0.0f;
+	CtrlRot.Roll=0.0f;
+	AddMovementInput(FRotationMatrix(CtrlRot).GetScaledAxis(EAxis::Y),value);
 }
 
 // Called every frame
@@ -51,6 +64,7 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAxis("MoveForward",this,&ASCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight",this,&ASCharacter::MoveRight);
 	
+	// Using Controller Rotation, whitch won't effect the character trans directly
 	PlayerInputComponent->BindAxis("Turn",this,&APawn::AddControllerYawInput);
 	PlayerInputComponent->BindAxis("LookUp",this,&APawn::AddControllerPitchInput);
 	
