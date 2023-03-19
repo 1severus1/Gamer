e6e9c30a9f623e72e0a659c931846c62c3cb66fa
severus <sf_lxyx@163.com>
2023-03-19 21:25:25 +0800

    UdemyGame

---
+++ UdemyGame/Source/UdemyGame/UdemyGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UdemyGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UdemyGame, "UdemyGame" );
---
+++ UdemyGame/Source/UdemyGame/UdemyGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UdemyGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMYGAME_API AUdemyGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
